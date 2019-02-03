253def8ac77c692d1ef3b5acaed7220238fef996
Sir-Apex <45063448+Sir-Apex@users.noreply.github.com>
2019-02-03 22:07:34 +0700

    Update main.c

--- 2 sem/avl_tree/main.c
+++ 2 sem/avl_tree/main.c
@@ -1,29 +1,104 @@
 #include <stdio.h>
+#include <malloc.h>
 
-typedef struct tree_node {///описание структуры узла дерева
-    int key;///КЛЮЧ УЗЛА
-    int height;///разница высот правого и левого дерева
-    struct tree_node*left;
-    struct tree_node*right;
 
+typedef  struct tree_node { ///узел
+    int key;
+    unsigned char height;///высота поддерева с корнем в данном узле
+    struct tree_node * left_node;
+    struct tree_node * right_node;
 }tree_node;
 
 
-int height(tree_node* elem){///получить высоту узла
-    return elem?elem->height:0;
+tree_node * tree_create(int key){
+    tree_node* node =malloc(sizeof(tree_node));
+    node->key=key;
+    node->height=1;
+    node->left_node=0;
+    node->right_node=0;
+    return node;
+}
+
+unsigned char get_height(tree_node* root){
+return (root) ? (root->height) : (0);
+}
+
+void count_height(tree_node*root){ ///получить высоту поддерева после балансировки
+unsigned char hl=get_height(root->left_node);
+unsigned char hr=get_height(root->right_node);
+root->height=((hl>hr) ? (hl) : (hr)) +1;
+}
+
+int balance_factor(tree_node* root) {
+return get_height(root->right_node)-get_height(root->left_node);///здесь исправил
 }
 
 
-int bfactor (tree_node* elem){///проверяет балансировку
-    return height(elem->right)-height(elem->left);
+tree_node* rotate_left(tree_node* root){
+    tree_node* new_root=root->right_node;
+    root->right_node=new_root->left_node;
+    new_root->left_node=root;
+    count_height(root);
+    count_height(new_root);
+return new_root;
 }
 
-void fix_height(tree_node* elem){
-    int h1=height(elem->left);
-    int h2=height(elem->right);
-    elem->height=(h1>h2?h1:h2)+1;
+
+tree_node* rotate_right(tree_node* root){
+    tree_node* new_root=root->left_node;
+    root->left_node=new_root->right_node;
+    new_root->right_node=root;
+    count_height(root);
+    count_height(new_root);
+ return new_root;
 }
+
+tree_node* balance(tree_node* root){
+    count_height(root);
+if (balance_factor(root)==2){
+    if (balance_factor(root->right_node)<0)
+        root->right_node=rotate_right(root->right_node);
+    return rotate_left(root);
+ }
+if (balance_factor(root)==-2){
+    if (balance_factor(root->left_node)>0)
+       root->left_node=rotate_left(root->left_node);
+    return rotate_right(root);
+ }
+ return root;
+ }
+
+
+tree_node* insert(tree_node *root,int k) {
+    if (root == 0) return tree_create(k);
+    if (k < root->key) root->left_node = insert(root->left_node, k);
+    else root->right_node = insert(root->right_node, k);
+    return balance(root);
+}
+
+
+    void tree_free(tree_node* tree)
+    {if (tree == NULL)
+            return;
+        tree_free(tree->left_node);
+        tree_free(tree->right_node);
+
+        free(tree);
+
+    }
+
+
+
 
 int main() {
+    tree_node * tree=NULL;
+    int n,cur;
+scanf("%d",&n);
+while(n--){
+    scanf("%d",&cur);
+    tree=insert(tree,cur);
+}
+printf("%d",get_height(tree));
+tree_free(tree);
     return 0;
 }
